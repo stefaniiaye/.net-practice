4a70d881870bb40503566e89b25359eeaebcc42a
s27591 <s27591@pjwstk.edu.pl>
2024-03-16 18:38:24 +0100

    Added more data for testing Avg method

--- practice/practice01/Program.cs
+++ practice/practice01/Program.cs
@@ -5,7 +5,10 @@ class Program
     static void Main(string[] args)
     {
         int[] arr1 = { 1, 2, 3, 4, 5,500 };
+        int[] arr2 = { 1, 2, 3, 4, 5,-500 };
+        
         Console.WriteLine(Avg(arr1));
+        Console.WriteLine(Avg(arr2));
     }
 
     public static float Avg(int[] arr)
--- practice/practice01/obj/Debug/net8.0/practice01.AssemblyInfo.cs
+++ practice/practice01/obj/Debug/net8.0/practice01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c15ffa500da900f72f37fe7ee5dd93bf4414a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1d3d4b57555cb2d9f795ecb93a195098bcdbe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
