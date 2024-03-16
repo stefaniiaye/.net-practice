58e15f3e7ddb240d57e9f6a6fa67ef2fd66df560
s27591 <s27591@pjwstk.edu.pl>
2024-03-16 19:48:00 +0100

    Added Max method

--- practice/practice01/Program.cs
+++ practice/practice01/Program.cs
@@ -7,8 +7,9 @@ class Program
         int[] arr1 = { 1, 2, 3, 4, 5,500 };
         int[] arr2 = { 1, 2, 3, 4, 5,-500 };
         
-        Console.WriteLine(Avg(arr1));
-        Console.WriteLine(Avg(arr2));
+        Console.WriteLine("average: " + Avg(arr1));
+        Console.WriteLine("average: " + Avg(arr2));
+        Console.WriteLine("max: " + Max(arr1));
     }
 
     public static float Avg(int[] arr)
@@ -19,5 +20,15 @@ class Program
             sum += i;
         avg = (float)sum / arr.Length;
         return avg;
+    }
+
+    public static int Max(int[] arr)
+    {
+        int max = int.MinValue;
+        foreach (int i in arr)
+        {
+            if (i > max) max = i;
+        }
+        return max;
     }
 }
