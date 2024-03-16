6d1d3d4b57555cb2d9f795ecb93a195098bcdbe9
s27591 <s27591@pjwstk.edu.pl>
2024-03-16 18:12:05 +0100

    Added Avg method

--- practice/practice01/Program.cs
+++ practice/practice01/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World:)))");
+        int[] arr1 = { 1, 2, 3, 4, 5,500 };
+        Console.WriteLine(Avg(arr1));
+    }
+
+    public static float Avg(int[] arr)
+    {
+        float avg = 0;
+        int sum = 0;
+        foreach (int i in arr)
+            sum += i;
+        avg = (float)sum / arr.Length;
+        return avg;
     }
 }
